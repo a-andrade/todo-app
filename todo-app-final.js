177ddcafcce535ba91d6d9f13ad72362bd79007d
Andres Andrade <a.andrade09@yahoo.com>
2021-04-04 18:55:00 -0700

    Added id field with uuidv4 library

--- todo-app-final.js
+++ todo-app-final.js
@@ -16,6 +16,7 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
 
     todos.push({
+        id: uuidv4(),
         text: e.target.elements.text.value,
         completed: false
     })
